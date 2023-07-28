f428ba5da7bb1e3e0b0494b0888cd2aa5bab9348
vijay <vijayravula9010@gmail.com>
2023-07-28 15:17:48 +0530

    added heading

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is vijay</h1>
+       <h1>This is vijay</h1>
+    </div>
+  );
+}
+
+export default App;
